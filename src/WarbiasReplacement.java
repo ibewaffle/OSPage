79c778e294a1d7c675c9bd951a45fdce16ac9b4f
Warwick Palm <warwick@tornstuff.com>
2011-10-03 23:42:10 +1300

    Created our placement .java file. Named it Warbias. Not sure what more to do yet, can't remember how to extend a class!

---
+++ src/WarbiasReplacement.java
@@ -0,0 +1,14 @@
+/*
+ * Warbias Algorithm
+ */
+
+/**
+ *
+ * @author Warwick Palm, Toby Strugnell
+ */
+public class WarbiasReplacement extends PageReplacement {
+    
+    //fucked if I know what's going on here, bloody hell
+    }
+    
+}
